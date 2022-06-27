8fc2419bf32bdea1ecfa1f1209138d4bd4cfaa1d
filoupegase <loisoncorentin@yahoo.fr>
2022-06-27 11:05:24 +0200

    feat[styled first style]

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
+import { styled } from './lib/styles/stitches.config';
+
 
 const App = () => {
-    return (
-        <>
-            <h1>
-                Hi there! I'm Coco
-                {/*<Wave>👋</Wave>*/}
-            </h1>
-            <h2>
-                I'm a frontend web developer based in Paris, France
-            </h2>
-        </>
-    );
+  return (
+      <>
+        <Heading1>
+          Hi there! I'm Coco
+          {/*<Wave>👋</Wave>*/ }
+        </Heading1>
+        <h2>
+          I'm a frontend web developer based in Paris, France
+        </h2>
+      </>
+  );
 }
+
+const Heading1 = styled("h1", {
+  margin: '0 0 0.5em -1px'
+})
 
 export default App;
---
+++ src/lib/styles/stitches.config.ts
@@ -0,0 +1,73 @@
+import { createStitches } from '@stitches/react';
+
+
+export const { styled, theme, createTheme } = createStitches({
+  theme: {
+    colors: {
+      backgroundInner: "#ffffff",
+      backgroundOuter: "#fcfcfc",
+     // backgroundHeader: hexToRgba("#fcfcfc", 0.7),
+      text: "#202020",
+      mediumDark: "#515151",
+      medium: "#5e5e5e",
+      mediumLight: "#757575",
+      light: "#d2d2d2",
+      kindaLight: "#e3e3e3",
+      superLight: "#f4f4f4",
+      superDuperLight: "#fbfbfb",
+      link: "#0e6dc2",
+     // linkUnderline: hexToRgba("#0e6dc2", 0.4),
+      success: "#44a248",
+      error: "#ff1b1b",
+      warning: "#f78200",
+      // Syntax Highlighting (light) - modified from Monokai Light: https://github.com/mlgill/pygments-style-monokailight
+      codeText: "#313131",
+      codeBackground: "#fdfdfd",
+      codeComment: "#656e77",
+      codeKeyword: "#029cb9",
+      codeAttribute: "#70a800",
+      codeNamespace: "#f92672",
+      codeLiteral: "#ae81ff",
+      codePunctuation: "#111111",
+      codeVariable: "#d88200",
+      codeAddition: "#44a248",
+      codeDeletion: "#ff1b1b",
+    },
+    radii: {
+      rounded: "0.65em"
+    }
+  },
+});
+
+export const darkTheme = createTheme({
+  colors: {
+    backgroundInner: "#1e1e1e",
+    backgroundOuter: "#252525",
+    //backgroundHeader: hexToRgba("#252525", 0.85),
+    text: "#f1f1f1",
+    mediumDark: "#d7d7d7",
+    medium: "#b1b1b1",
+    mediumLight: "#959595",
+    light: "#646464",
+    kindaLight: "#535353",
+    superLight: "#272727",
+    superDuperLight: "#1f1f1f",
+    link: "#88c7ff",
+    //linkUnderline: hexToRgba("#88c7ff", 0.4),
+    success: "#78df55",
+    error: "#ff5151",
+    warning: "#f2b702",
+    // Syntax Highlighting (dark) - modified from Dracula: https://github.com/dracula/pygments
+    codeText: "#e4e4e4",
+    codeBackground: "#212121",
+    codeComment: "#929292",
+    codeKeyword: "#3b9dd2",
+    codeAttribute: "#78df55",
+    codeNamespace: "#f95757",
+    codeLiteral: "#d588fb",
+    codePunctuation: "#cccccc",
+    codeVariable: "#fd992a",
+    codeAddition: "#78df55",
+    codeDeletion: "#ff5151",
+  },
+});
