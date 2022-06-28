c5b1c96945f439cc82036db91ac6832339038530
filoupegase <loisoncorentin@yahoo.fr>
2022-06-28 17:19:27 +0200

    feat[external link component]

--- src/_common/components/Link/Link.tsx
+++ src/_common/components/Link/Link.tsx
@@ -1,6 +1,7 @@
+import type { ComponentProps } from 'react';
 import NextLink from "next"
-import { Component, ComponentProps } from 'react';
 import { styled } from '../../../lib/styles/stitches.config';
+import { baseUrl } from "../../../lib/config"
 
 
 const StyledLink = styled(NextLink, {
@@ -11,30 +12,57 @@ const StyledLink = styled(NextLink, {
     underline: {
       // nice animated link underline effect
       true: {
-        setUnderlineVars: {}
-      }
+        setUnderlineVars: {},
+        // underline height is based on link's font size
+        $$underlineSize: "calc(0.1em + 0.05rem)",
+
+        backgroundImage: `linear-gradient($$underlineColor, $$underlineColor)`,
+        backgroundPosition: "0% 100%",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "0% $$underlineSize",
+        paddingBottom: "0.2rem",
+
+        "@media (prefers-reduced-motion: no-preference)": {
+          transition: "background-size 0.25s ease-in-out",
+        },
+
+        "&:hover": {
+          backgroundSize: "100% $$underlineSize",
+        }
+      },
+      false: {}
     }
   }
-})
+});
 
 export type LinkProps = ComponentProps<typeof StyledLink> & {
   openInNewTab?: boolean
 }
 
-class Link extends Component<LinkProps> {
-  static defaultProps = { prefetch: false, underline: true }
+// Ce composant détecte automatiquement si ce lien doit s'ouvrir dans la même fenêtre
+// (par défaut pour les liens internes) ou dans un nouvel onglet
 
-  render() {
-    let { href, rel, target, prefetch, underline, openInNewTab, ...rest }: Readonly<any> = this.props;
+// @ts-ignore
+const Link = ({ href, rel, target, prefetch = false, underline = true, openInNewTab, ...rest }: LinkProps) => {
+  // @ts-ignore
+  const isExternal = typeof href === "string" && !(href.startsWith("/") || href.startsWith("#") || href.startsWith(baseUrl));
+
+  if (openInNewTab || isExternal) {
     return (
         <StyledLink
             href={ href }
-            target={ target || "_blank" }
+            target={ target || "_black" }
+            rel={ `${ rel || '' } noopener ${ isExternal ? "noreferrer" : "" }`.trim() }
             underline={ underline }
             { ...rest }
         />
-    )
+    );
   }
+  return (
+      <StyledLink
+          { ...{ href, rel, target, prefetch, underline, ...rest } }
+      />
+  );
 }
 
 export default Link;
