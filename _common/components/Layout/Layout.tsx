20dc006de0111949344fcf2eb0c833e0db7f9f4e
filoupegase <loisoncorentin@yahoo.fr>
2022-07-06 16:20:10 +0200

    feat::Layout embording _cmp

--- _common/components/Layout/Layout.tsx
+++ _common/components/Layout/Layout.tsx
@@ -1,21 +1,86 @@
 import { ComponentProps } from 'react';
+import Head from 'next/head';
 import { styled } from "../../../lib/styles/stitches.config"
+import Header from '../Header';
 
 
 const Flex = styled('div', {
   display: "flex",
   flexDirection: 'column',
   minHeight: '100vh'
+});
+
+const Default = styled("main", {
+  width: "100",
+  padding: "1.5em"
+});
+
+const ContainerDiv = styled("div", {
+  maxWidth: "865px",
+  margin: "0 auto",
+  display: "block"
+});
+
+const StickyHeader = styled(Header, {
+  position: "sticky",
+  top: 0
+});
+
+/*
+const FlexedFooter = styled(Footer, {
+  flex: 1
 })
+*/
+
+const SkipNavLink = styled("a", {
+  // accessible invisibility stuff pulled from @reach/skip-nav:
+  // https://github.com/reach/reach-ui/blob/main/packages/skip-nav/styles.css
+  border: 0,
+  clip: "rect(0 0 0 0)",
+  height: "1px",
+  width: "1px",
+  margin: "-1px",
+  padding: 0,
+  overflow: "hidden",
+  position: "absolute",
+
+  "&:focus": {
+    padding: "1rem",
+    position: "fixed",
+    top: "10px",
+    left: "10px",
+    zIndex: 99999,
+    width: "auto",
+    height: "auto",
+    clip: "auto",
+    background: "$superDuperLight",
+    color: "$link",
+    border: "2px solid $kindaLight",
+    borderRadius: "$rounded",
+    textDecoration: "underline",
+  },
+});
 
 
 export type LayoutProps = ComponentProps<typeof Flex> & {
-  container?: boolean// pass false to disable default `<main>` container styles with padding... etc.
+  container?: boolean // pass false to disable default `<main>` container styles with padding... etc.
 }
 
 const Layout = ({ container = true, children, ...rest }: LayoutProps) => {
+  //const { activeTheme } = useTheme();
+  const skipNavId = "skip-nav";
+
   return (
-    <p>salut Layout</p>
+    <>
+      <Head>
+
+      </Head>
+      <Flex { ...rest }>
+        <Default>
+          <ContainerDiv>{ children }</ContainerDiv>
+        </Default>
+      </Flex>
+    </>
   )
 }
 
