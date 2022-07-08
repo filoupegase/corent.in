68a67a0febd977790565ce5af3a13b034cf8c96d
filoupegase <loisoncorentin@yahoo.fr>
2022-07-08 11:47:10 +0200

    feat :: footer

--- _common/components/Footer/Footer.tsx
+++ _common/components/Footer/Footer.tsx
@@ -1,11 +1,27 @@
 import type { ComponentProps } from "react";
+import { styled, theme, keyframes } from "../../../lib/styles/stitches.config";
 
 
-export type FooterProps = ComponentProps<any>;
+const Wrapper = styled('footer', {
+  width: "100%",
+  padding: "1.25em 1.5em",
+  backgroundColor: theme.colors.backgroundOuter,
+  color: theme.colors.mediumDark,
+  transition: `background ${ theme.transitions.fade }, border ${ theme.transitions.fade }`,
+
+  "@medium": {
+    padding: "1em 1.25em",
+  },
+});
+
+
+export type FooterProps = ComponentProps<typeof Wrapper>;
 
 const Footer = ({ ...rest }: FooterProps) => {
   return (
-    <p>salut</p>
+    <Wrapper { ...rest }>
+
+    </Wrapper>
   )
 }
 
