4abaecc05681bc83bebab7bc35b2db256c46a273
filoupegase <loisoncorentin@yahoo.fr>
2022-07-11 13:48:01 +0200

    feat :: Menu Items

--- _common/components/Menu/Menu.tsx
+++ _common/components/Menu/Menu.tsx
@@ -1,4 +1,5 @@
 import type { ComponentProps } from "react";
+import { useRouter } from "next/router";
 import { styled } from '../../../lib/styles/stitches.config';
 
 
@@ -16,13 +17,32 @@ const Wrapper = styled("ul", {
   "@small": {
     marginLeft: "1.4em"
   },
+});
+
+const Item = styled("li", {
+  display: "inline-block",
+  marginLeft: "1em",
+  listStyle: "none",
+
+  "@meduim": {
+    marginLeft: 0
+  },
+
+  "@small": {
+    // the home icon is kinda redundant when space is SUPER tight
+    "&:first-of-type": {
+      display: "none",
+    },
+  }
 })
 
 export type MenuProps = ComponentProps<typeof Wrapper>;
 
 const Menu = ({ ...rest }: MenuProps) => {
+  const router = useRouter();
+
   return (
-    <Wrapper>
+    <Wrapper { ...rest }>
 
     </Wrapper>
   )
