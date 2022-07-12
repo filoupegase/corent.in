7ad48c83e868f6cea2d9ba181db57714a93e73f4
filoupegase <loisoncorentin@yahoo.fr>
2022-07-12 23:58:14 +0200

    config next

--- lib/config/constants.ts
+++ lib/config/constants.ts
@@ -1,8 +1,7 @@
 // Next.js constants (not needed in frontend)
-import path from 'path';
 
 // directory containing .mdx files relative to project root
-export const NOTES_DIR = path.join(process.cwd(), 'notes');
+export const NOTES_DIR = "notes";
 
 // normalize the timestamp saved when building/deploying (see next.config.js) and fall back to right now:
-export const RELEASE_DATE = new Date(process.env.NEXT_PUBLIC_RELEASE_DATE || Date.now()).toISOString();
+export const RELEASE_DATE = new Date(process.env.RELEASE_DATE || Date.now()).toISOString();
