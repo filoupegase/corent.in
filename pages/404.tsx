e650214c536ef9f67d0aa5e3606da09c4dec1648
filoupegase <loisoncorentin@yahoo.fr>
2022-07-14 13:39:45 +0200

    feat:404 not found

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,28 @@
+import { styled, theme } from "../lib/styles/stitches.config"
+
+
+const Center = styled('div', {
+  textAlign: "center"
+});
+
+const H1 = styled("h1", {
+  fontSize: "1.8em",
+  fontWeight: 500,
+  color: theme.colors.text,
+
+  "@medium": {
+    fontSize: "1.6em",
+  },
+});
+
 const FourOhFour = () => {
-  return <p>404</p>;
+  return (
+    <>
+      <Center>
+        <H1>404: Page Not Found 😢</H1>
+      </Center>
+    </>
+  );
 };
 
 export default FourOhFour;
