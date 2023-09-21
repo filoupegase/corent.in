3d09f11544a1f2ae7787c0c4f92301579cc17370
filoupegase <loisoncorentin@yahoo.fr>
2023-09-21 15:25:48 +0200

    fix/req unuse

--- pages/api/count.ts
+++ pages/api/count.ts
@@ -1,11 +1,10 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 
 export const config = {
   runtime: "edge",
   regions: ["iad1"], // the vercel postgres database lives in DC
 };
 
-export default async (req: NextRequest) => {
+export default async () => {
   return NextResponse.json({ message: "Hello from Next.js!" });
 };
