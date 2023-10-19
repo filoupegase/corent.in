c381c4da1bf37be5b6157908030f994eaecf44a8
filoupegase <loisoncorentin@yahoo.fr>
2023-10-19 16:42:00 +0200

    feature/finish my contact form

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,5 +1,6 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import queryString from "query-string";
+import type { NextRequest } from "next/server";
 
 // fallback to dummy secret for testing: https://docs.hcaptcha.com/#integration-testing-test-keys
 const HCAPTCHA_SITE_KEY =
@@ -7,19 +8,23 @@ const HCAPTCHA_SITE_KEY =
 const HCAPTCHA_SECRET_KEY = process.env.HCAPTCHA_SECRET_KEY || "0x0000000000000000000000000000000000000000";
 const HCAPTCHA_API_ENDPOINT = "https://hcaptcha.com/siteverify";
 
+const { AIRTABLE_API_KEY, AIRTABLE_BASE } = process.env;
+const AIRTABLE_API_ENDPOINT = "https://api.airtable.com/v0/";
+
 export const config = {
   runtime: "edge",
 };
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextRequest) => {
-  // redirect GET requests to this endpoint to the contact form itself
   if (req.method === "GET") {
     return NextResponse.redirect(`${process.env.BASE_URL}/contact/`);
   }
 
   const data = await req.json();
 
+  // these are both backups to client-side validations just in case someone squeezes through without them. the codes
+  // are identical so they're caught in the same fashion.
   if (!data.name || !data.email || !data.message) {
     // all fields are required
     throw new Error("MISSING_DATA");
@@ -28,6 +33,31 @@ export default async (req: NextRequest) => {
     // either the captcha is wrong or completely missing
     throw new Error("INVALID_CAPTCHA");
   }
+
+  // sent directly to airtable
+  const airtableResult = await sendToAirtable({
+    Name: data.name,
+    Email: data.email,
+    Message: data.message,
+  });
+
+  // throw an internal error, not user's fault
+  if (airtableResult !== true) {
+    throw new Error("AIRTABLE_API_ERROR");
+  }
+
+  // success! let the client know
+  return NextResponse.json(
+    { success: true },
+    {
+      status: 201,
+      headers: {
+        // disable caching on both ends. see:
+        // https://vercel.com/docs/concepts/functions/edge-functions/edge-caching
+        "Cache-Control": "private, no-cache, no-store, must-revalidate",
+      },
+    }
+  );
 };
 
 const validateCaptcha = async (formResponse: unknown): Promise<unknown> => {
@@ -46,4 +76,19 @@ const validateCaptcha = async (formResponse: unknown): Promise<unknown> => {
   const result = await response.json();
 
   return result.success;
+};
+
+const sendToAirtable = async (data: unknown): Promise<boolean> => {
+  const response = await fetch(`${AIRTABLE_API_ENDPOINT}${AIRTABLE_BASE}/Projets`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${AIRTABLE_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      fields: data,
+    }),
+  });
+
+  return response.ok;
 };
