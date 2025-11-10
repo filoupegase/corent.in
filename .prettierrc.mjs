15613ac37fdf2f70f51db4949b0325344b903a63
filoupegase <loisoncorentin@yahoo.fr>
2025-11-10 10:48:44 +0100

    chore(node22) bump node and many many deps

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,6 +1,7 @@
 /** @type {import("prettier").Config} */
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
+  tailwindFunctions: ["cn", "clsx", "cva", "twMerge"],
   jsxSingleQuote: false,
   printWidth: 120,
   quoteProps: "as-needed",
